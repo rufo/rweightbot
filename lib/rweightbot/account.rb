89bc5d7751f64579cd03f7ebd71307585fedeb0b
Rufo Sanchez <rufo@rufosanchez.com>
2012-12-15 18:03:16 -0500

    added YARD documentation for methods

--- lib/rweightbot/account.rb
+++ lib/rweightbot/account.rb
@@ -5,19 +5,30 @@ module RWeightbot
   class Account
     attr_accessor :email, :password
 
-    def initialize(options)
+    # Creates a new Account object. Note that it won't try to log in until you call one of the other public methods.
+    # @param [Hash] options Options for creating an account:
+    # @option options [String] :email The e-mail address for a registered account at weightbot.com
+    # @option options [String] :password The password for the same account
+    def initialize(options = {})
       self.email = options[:email]
       self.password = options[:password]
     end
 
+    # Attempts to log in at weightbot.com, and returns true if the credentials were valid.
+    # @return [Boolean] Whether or not the login was successful.
     def valid_credentials?
       !export_form.nil?
     end
 
+    # Attempts to log in at weightbot.com, and returns the CSV data file as a String.
+    # @return [String] The weightbot_data.csv file, in its entirety.
     def csv
       mech.submit(export_form).body
     end
 
+    # Attempts to log in at weightbot.com, downloads the CSV file, and parses it into
+    # an array of {Weighing} objects.
+    # @return [Array<Weighing>] An array of Weighing objects.
     def weighings
       return @weighings if @weighings
 
--- lib/rweightbot/weighing.rb
+++ lib/rweightbot/weighing.rb
@@ -2,6 +2,12 @@ module RWeightbot
   class Weighing
     attr_accessor :date, :kilograms, :pounds
 
+    # Initializes a new Weighing object. Note that the Account object will return
+    # an array of these for you, so you shouldn't need to do this directly.
+    # @param [Hash] options The keys for the Weighing object attributes.
+    # @option options [String] :date The date of the weighing, in "YYYY-MM-DD" format. This gets converted to a Date object on assignment.
+    # @option options [#to_f] :kilograms The weight, in kilograms. This gets converted to a Float object on assignment.
+    # @option options [#to_f] :pounds The weight, in pounds. This gets converted to a Float object on assignment.
     def initialize(options = {})
       options.each {|name, value| send("#{name}=", value)}
     end
