c260e46dab2c3bd7e879a1ffbfbf9af803eb8e18
Rufo Sanchez <rufo@rufosanchez.com>
2012-12-15 16:51:19 -0500

    added weighing spec and implementation

--- lib/rweightbot/weighing.rb
+++ lib/rweightbot/weighing.rb
@@ -1,5 +1,21 @@
 module RWeightbot
   class Weighing
+    attr_accessor :date, :kilograms, :pounds
 
+    def initialize(options = {})
+      options.each {|name, value| send("#{name}=", value)}
+    end
+
+    def date=(new_date)
+      @date = Date.strptime(new_date, "%Y-%m-%d")
+    end
+
+    def kilograms=(new_kilograms)
+      @kilograms = new_kilograms.to_f
+    end
+
+    def pounds=(new_pounds)
+      @pounds = new_pounds.to_f
+    end
   end
 end
--- spec/rweightbot_weighing_spec.rb
+++ spec/rweightbot_weighing_spec.rb
@@ -1,5 +1,17 @@
 require "spec_helper"
 
 describe RWeightbot::Weighing do
+  subject{RWeightbot::Weighing.new(:date => "2012-01-01", :kilograms => "81.7", :pounds => "180.2")}
 
+  it "should have a date" do
+    subject.date.should eq(Date.new(2012, 1, 1))
+  end
+
+  it "should have the weight in pounds" do
+    subject.pounds.should eq(180.2)
+  end
+
+  it "should have the weight in kilograms" do
+    subject.kilograms.should eq(81.7)
+  end
 end
